66ca1ba69dd12722ac207657e8fac169b308bdbd
Dani <Dani@Home>
2015-04-18 11:52:24 +0200

    Definidas las pruebas para movimiento (definidas la primera vez que se realizó el proyecto)

--- src/test/MoveCardControllerTest.java
+++ src/test/MoveCardControllerTest.java
@@ -1,5 +1,74 @@
 package test;
 
+import org.junit.Test;
+
 public class MoveCardControllerTest {
+	@Test
+	public void moveFromWasteToFoundationTest(){
+
+		//No cards in waste
+		
+		
+		//No cards in foundation & A in waste (exist Foundation suit)
+		
+		
+		//No cards in foundation & A in waste (no exist Foundation suit)
+		
+		
+		//No cards in foundation & no A in waste
+		
+		
+		//Waste card consecutive to Foundation card
+		
+		
+		//Waste card  no consecutive to Foundation card
+		
+		
+		//Waste card consecutive to Foundation card but different suit
+		
+		
+	}
+	
+	@Test
+	public void moveFromDeckToWasteTest(){
+		
+		//All cards in Deck, no cards in Waste
+
+		
+		//All cards in Waste, no cards in Deck
+	
+		
+		//One card in Deck, rest in Waste
+
+		
+		//Two cards in Deck, rest in Waste
+	
+		
+		//Three cards in Deck, rest in Waste
+	
+		
+		//More than Three cards in Deck, rest in Waste		
+
+	}
+	
+	@Test
+	public void moveFromWasteToTableauTest(){
+		
+		//No cards in Waste
+
+
+		//No cards in Tableau, score card different to ROI in Waste
+
+		
+		//No cards in Tableau,  card ROI in Waste
+
+		
+		//One score lower card in Waste that tableau (same color)	
+
+
+		//One score lower card in Waste that tableau (different color)
+
+		
+	}
 
 }
