15a8a28cd7bfba3e6ce1c51eb3eedb3e5cb30c3a
Dani <Dani@Home>
2015-04-18 11:57:50 +0200

    Definidos los test para StartController (pensadas en el Workspace antiguo)

--- src/test/StartControllerTest.java
+++ src/test/StartControllerTest.java
@@ -1,5 +1,32 @@
 package test;
 
-public class StartControllerTest {
+import org.junit.Before;
+import org.junit.Test;
 
+public class StartControllerTest {
+	@Before
+    public void before(){
+    }
+    
+    @Test
+    public void getSizeDeckTest() {
+    }
+    
+    
+    @Test
+    public void getSizeWasteTest() {
+    }
+    
+    
+    @Test
+    public void getFoundationsTest() {
+
+    }
+    
+    
+    @Test
+    public void getTableauTest() {
+    	
+		}
+	
 }
